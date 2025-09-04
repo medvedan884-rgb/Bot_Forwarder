c4520119e7bf904a658a24a000ed9950e881260f
medvedan884-rgb <medvedan884@gmail.com>
2025-09-05 00:00:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from telethon import TelegramClient, events
+from flask import Flask
 import os
+import threading
 
-# данные из переменных окружения
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 target_chat = int(os.getenv("TARGET_CHAT"))
@@ -13,6 +14,18 @@ client = TelegramClient("user", api_id, api_hash)
 async def handler(event):
     await event.forward_to(target_chat)
 
-print("✅ Userbot запущен и ждёт сообщения...")
-client.start()
-client.run_until_disconnected()
+# фейковый веб-сервер (чтобы Render держал сервис живым)
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Bot is running!"
+
+def run_flask():
+    app.run(host="0.0.0.0", port=10000)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_flask).start()
+    print("✅ Userbot запущен и ждёт сообщения...")
+    client.start()
+    client.run_until_disconnected()
